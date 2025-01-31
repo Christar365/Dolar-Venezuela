9b710d0b21da5269be04517c10f69c07461e1469
Christar365 <crisram0108@gmail.com>
2025-01-31 14:41:04 -0400

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,26 @@
+// Este es el service worker para la página offline
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
 const CACHE_NAME = 'dolarve-cache-v1';
+const CACHE = "pwabuilder-page";
+const offlineFallbackPage = "/offline.html";
+
 const urlsToCache = [
   '/',
   '/index.html',
   '/assets/styles.css',
   '/assets/app.js',
   '/assets/icon.png',
-  '/manifest.json'
+  '/manifest.json',
+  offlineFallbackPage
 ];
 
-// Instalación del Service Worker y almacenamiento en caché de los archivos esenciales
-self.addEventListener('install', event => {
+// Instalación del Service Worker
+self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Archivos en caché');
+        console.log('Archivos en caché correctamente');
         return cache.addAll(urlsToCache);
       })
   );
@@ -21,6 +28,7 @@ self.addEventListener('install', event => {
 
 // Activación del Service Worker
 self.addEventListener('activate', event => {
+  console.log('Service Worker activado');
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -33,11 +41,38 @@ self.addEventListener('activate', event => {
       );
     })
   );
+  if (workbox.navigationPreload.isSupported()) {
+    workbox.navigationPreload.enable();
+  }
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
 // Intercepción de solicitudes de red y entrega desde caché cuando sea posible
-self.addEventListener('fetch', event => {
-  if (event.request.url.includes('/v1/dolares')) {
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  } else if (event.request.url.includes('/v1/dolares')) {
     event.respondWith(
       fetch(event.request)
         .then(networkResponse => {
