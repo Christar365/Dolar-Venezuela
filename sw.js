77fb93cd4821a4e86217ffed800fad9022d2b7de
Christar365 <crisram0108@gmail.com>
2025-01-31 13:25:12 -0400

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,60 @@
-const CACHE_NAME = 'dolar-ve-cache-v1';
+const CACHE_NAME = 'dolarve-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   '/assets/styles.css',
   '/assets/app.js',
-  '/assets/icon.png' // Agrega el icono cuando lo tengas
+  '/assets/icon.png',
+  '/manifest.json'
 ];
 
-// Durante la fase de instalación, se almacena en caché los recursos estáticos
-self.addEventListener('install', (event) => {
+// Instalación del Service Worker y almacenamiento en caché de los archivos esenciales
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
+      .then(cache => {
+        console.log('Archivos en caché');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Intercepta las solicitudes y responde con recursos de la caché si están disponibles
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response; // Si está en caché, responde con ello
-        }
-        return fetch(event.request); // Si no, realiza una solicitud de red
-      })
+// Activación del Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
+});
+
+// Intercepción de solicitudes de red y entrega desde caché cuando sea posible
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('/v1/dolares')) {
+    event.respondWith(
+      fetch(event.request)
+        .then(networkResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request.url, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => {
+          return caches.match(event.request);
+        })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
